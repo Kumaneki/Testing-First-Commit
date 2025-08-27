c29133cee63f19126be51f6d4cfde0440b3a0c76
Kumaneki <mghanighazali@gmail.com>
2025-08-27 09:49:08 +0700

    Tugas BKPM 1

---
+++ Kelipatan2.php
@@ -0,0 +1,10 @@
+<?php
+echo "Kelipatan 2\n";
+
+$x = 1;
+
+for ($i = 1; $i <= 10; $i++) {
+    $x = $x * 2;
+    echo "$x\n";
+}
+?>
---
+++ Kelipatan2min1.php
@@ -0,0 +1,11 @@
+<?php
+echo "Kelipatan 2 dikurangi 1\n";
+
+$x = 12;
+
+for ($i = 1; $i <= 10; $i++) {
+    $x = $x * 2;
+
+    echo ($x - 1) . "\n";
+}
+?>
